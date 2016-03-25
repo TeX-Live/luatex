73dc4c70beaeb0bdc918f7e61a646f8db0d2105c
Luigi Scarso <luigi.scarso@gmail.com>
2016-03-25 21:23:32 +0000

    new beta-0.90.0

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# $Id$
+# $Id: build.sh 5833 2016-03-10 10:46:12Z luigi $
 #
 # Copyright (c) 2005-2011 Martin Schröder <martin@luatex.org>
 # Copyright (c) 2009-2014 Taco Hoekwater <taco@luatex.org>
